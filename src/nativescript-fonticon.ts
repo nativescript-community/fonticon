51317283a8bc6da57e77dbb4f1ebc7297204a5ad
Martin Guillon <martin.guillon@akylas.fr>
2019-04-06 15:56:34 +0200

    allow loading from required css data (webpack) . Removes the need for readTextSync

--- src/nativescript-fonticon.ts
+++ src/nativescript-fonticon.ts
@@ -1,70 +1,84 @@
-import {knownFolders} from 'tns-core-modules/file-system';
+import { knownFolders } from 'tns-core-modules/file-system';
+import { profile } from 'tns-core-modules/profiling';
 import * as lib from './lib';
 
 export class TNSFontIcon {
-  public static css: any = {}; // font icon collections containing maps of classnames to unicode
-  public static paths: any = {}; // file paths to font icon collections
-  public static debug: boolean = false;
+    public static css: any = {}; // font icon collections containing maps of classnames to unicode
+    public static paths: { [k: string]: string | object } = {}; // file paths to font icon collections
+    public static debug: boolean = false;
 
-  public static loadCss(): Promise<any> {
-    let cnt = 0;
-    let currentName: string;
-    const fontIconCollections = Object.keys(TNSFontIcon.paths);
-    if (TNSFontIcon.debug) {
-      console.log(`Collections to load: ${fontIconCollections}`);
-    }
-
-    const initCollection = () => {
-      currentName = fontIconCollections[cnt];
-      TNSFontIcon.css[currentName] = {};
-    };
-
-    const loadFile = (path: string): Promise<any> => {
-      if (TNSFontIcon.debug) {
-        console.log('----------');
-        console.log(`Loading collection '${currentName}' from file: ${path}`);
-      }
-      const cssFile = knownFolders.currentApp().getFile(path);
-      return new Promise((resolve, reject) => {
-        cssFile.readText().then((data) => {
-          const map = lib.mapCss(data, TNSFontIcon.debug);
-          TNSFontIcon.css[currentName] = map;
-          resolve();
-        }, (err) => {
-          reject(err);
-        });
-      });
-    };
-
-    const loadFiles = (): Promise<any> => {
-      return new Promise((resolve) => {
-        initCollection();
-
-        if (cnt < fontIconCollections.length) {
-          loadFile(TNSFontIcon.paths[currentName]).then(() => {
-            cnt++;
-            return loadFiles().then(() => {
-              resolve();
+    private static loadFile = profile(
+        'loadCSS',
+        (name: string, path: string): Promise<any> => {
+            if (TNSFontIcon.debug) {
+                console.log('----------');
+                console.log(`Loading collection '${name}' from file: ${path}`);
+            }
+            const cssFile = knownFolders.currentApp().getFile(path);
+            return new Promise((resolve, reject) => {
+                cssFile.readText().then(
+                    data => {
+                        const map = lib.mapCss(data, TNSFontIcon.debug);
+                        TNSFontIcon.css[name] = map;
+                        resolve();
+                    },
+                    err => {
+                        reject(err);
+                    }
+                );
             });
-          });
-        } else {
-          resolve();
         }
-      });
-    };
+    );
+    private static parseAndMapCSS = profile('parseAndMapCSS', (name: string, data: string) => {
+        if (TNSFontIcon.debug) {
+            console.log('----------');
+            console.log(`Loading collection '${name}' from data ${data}`);
+        }
+        TNSFontIcon.css[name] = lib.mapCss(data, TNSFontIcon.debug);
+    });
 
-    return loadFiles();
-  }
+    public static loadCss = (): Promise<any> => {
+        if (TNSFontIcon.debug) {
+            console.log(`Collections to load: ${TNSFontIcon.paths}`);
+        }
+        // return new Promise(() => {
+        return Promise.all(
+            Object.keys(TNSFontIcon.paths).map(currentName => {
+                TNSFontIcon.css[currentName] = {};
+                const data = TNSFontIcon.paths[currentName];
+                if (!data) {
+                    return;
+                }
+                if (typeof data === 'string') {
+                    return TNSFontIcon.loadFile(currentName, data);
+                } else {
+                    return TNSFontIcon.parseAndMapCSS(currentName, data[0][1]);
+                }
+            })
+        );
+
+        // if (cnt < fontIconCollections.length) {
+        //     loadFile(TNSFontIcon.paths[currentName]).then(() => {
+        //         cnt++;
+        //         return loadFiles().then(() => {
+        //             resolve();
+        //         });
+        //     });
+        // } else {
+        //     resolve();
+        // }
+        // });
+    }
 }
 
 export function fonticon(value: string): string {
-  if (value) {
-    if (value.indexOf('-') > -1) {
-      const prefix = value.split('-')[0];
-      return TNSFontIcon.css[prefix][value];
-    } else {
-      console.log('Fonticon classname did not contain a prefix. i.e., \'fa-bluetooth\'');
+    if (value) {
+        if (value.indexOf('-') > -1) {
+            const prefix = value.split('-')[0];
+            return TNSFontIcon.css[prefix][value];
+        } else {
+            console.log("Fonticon classname did not contain a prefix. i.e., 'fa-bluetooth'");
+        }
     }
-  }
-  return value;
+    return value;
 }
