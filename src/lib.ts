3181b208e35b60593bad18727bbdf34c61f8704f
Martin Guillon <martin.guillon@akylas.fr>
2019-04-06 16:09:13 +0200

    ignore non content selectors

--- src/lib.ts
+++ src/lib.ts
@@ -1,36 +1,40 @@
 // import { parse } from 'tns-core-modules/css';
 import { profile } from 'tns-core-modules/profiling';
 
-export const mapCss = profile('mapCss', (data: string, debug?: boolean): object => {
-    const map = {};
-    // const test = parse(data, {});
-    // console.log(test);
-    const sets = data.split('}');
+export const mapCss = profile(
+    'mapCss',
+    (data: string, debug?: boolean): object => {
+        const map = {};
+        // const test = parse(data, {});
+        // console.log(test);
+        const sets = data.split('}');
 
-    for (const set of sets) {
-        const pair = set.split('{');
-        if (pair[1]) {
-            const keyGroups = pair[0];
-            const keys = keyGroups.split(',');
-            const value = cleanValue(pair[1]);
-            if (!value) {
-                continue;
-            }
-            const realVal = String.fromCharCode(parseInt(value, 16));
-            for (let key of keys) {
-                key = key
-                    .trim() // remove spaces
-                    .slice(1) // remove the .
-                    .split(':before')[0]; // remove :before and anything after
-                map[key] = realVal;
-                if (debug) {
-                    console.log(`${key}: ${value} ${realVal}`);
+        for (const set of sets) {
+            const pair = set.split('{');
+            const preVal = pair[1];
+            if (preVal && preVal.indexOf('content:') !== -1) {
+                const keyGroups = pair[0];
+                const keys = keyGroups.split(',');
+                const value = cleanValue(preVal);
+                if (!value) {
+                    continue;
+                }
+                const realVal = String.fromCharCode(parseInt(value, 16));
+                for (let key of keys) {
+                    key = key
+                        .trim() // remove spaces
+                        .slice(1) // remove the .
+                        .split(':before')[0]; // remove :before and anything after
+                    map[key] = realVal;
+                    if (debug) {
+                        console.log(`${key}: ${value} ${realVal}`);
+                    }
                 }
             }
         }
+        return map;
     }
-    return map;
-});
+);
 
 export const cleanValue = (val: string): string | void => {
     const array = val.trim().split('"');
